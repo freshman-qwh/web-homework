134ba5df7784edbef77a325f3fe72e4da26b35c3
freshman-qwh <676628179@qq.com>
2022-12-12 18:26:09 +0800

    修改了弹窗大小及动画

--- js/popup.js
+++ js/popup.js
@@ -1,23 +1,24 @@
 $(document).ready(function(){
-    $(".popup").hide();                 /*设置了开局透明度为0可以不用hide和show，懒得改了*/
+    $(".popup").hide();
     $(".bg").hide();
+    $(".popup").animate({top:'-300px',opacity:'0'});
     // 点击按钮打开弹窗
     $("#pop_btn").click(function(){
             $(".popup").show();
-            $(".popup").animate({top:'0',opacity:'1'});
+            $(".popup").animate({top:'-100px',opacity:'1'});
             $(".bg").show();
     });
     // 点击 <span> (x), 关闭弹窗
     $(".close").click(function(){
         $(".popup").hide();
-        $(".popup").animate({top:'-200px',opacity:'0'});
+        $(".popup").animate({top:'-300px',opacity:'0'});
         $(".bg").hide();
     });
 
     // 在用户点击其他地方时，关闭弹窗
     $(".bg").click(function(){
         $(".popup").hide();
-        $(".popup").animate({top:'-200px',opacity:'0'});
+        $(".popup").animate({top:'-300px',opacity:'0'});
         $(".bg").hide();
     });
 });
